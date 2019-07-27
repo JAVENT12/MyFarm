45684d201aee73ad86f26eeb2ad216be1abb61f5
Jason Ventura <ventj2014@gmail.com>
2019-07-27 13:27:57 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFarm
+{
+    public class Cow
+    {
+        public string name;
+        public string sound;
+        public string food;
+        public string product;
+        public Cow(string aName, string aSound, string aFood, string aProduct)
+        {
+            name = aName;
+            sound = aSound;
+            food = aFood;
+            product = aProduct;
+        }
+        public void Intro()
+        {
+            Console.WriteLine($"\n\tHi my name is {name}, whenever I'm hungry I like to {sound}");
+        }
+        public void Speak()
+        {
+            Console.WriteLine($"\n\t{sound} out the way!");
+        }
+        public void Product()
+        {
+            Console.WriteLine($"\n\tI love making {product}");
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"\n\t{food} is my favorite snack");
+        }
+    }
+    public class Horse
+    {
+        public string name;
+        public string sound;
+        public string food;
+        public string product;
+        public Horse(string aName, string aSound, string aFood, string aProduct)
+        {
+            name = aName;
+            sound = aSound;
+            food = aFood;
+            product = aProduct;
+        }
+        public void Intro()
+        {
+            Console.WriteLine($"\n\t{sound} my name is {name} lets grab some {product} ");
+        }
+        public void Speak()
+        {
+            Console.WriteLine($"\n\t{sound} out the way!");
+        }
+        public void Product()
+        {
+            Console.WriteLine($"\n\tI love making {product}");
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"\n\t{food} is my favorite snack");
+        }
+    }
+    public class Chicken
+    {
+        public string name;
+        public string sound;
+        public string food;
+        public string product;
+        public Chicken(string aName, string aSound, string aFood, string aProduct)
+        {
+            name = aName;
+            sound = aSound;
+            food = aFood;
+            product = aProduct;
+        }
+        public void Intro()
+        {
+            Console.WriteLine($"\n\t{sound} my name is {name} lets grab some {product} ");
+        }
+        public void Speak()
+        {
+            Console.WriteLine($"\n\t{sound} out the way!");
+        }
+        public void Product()
+        {
+            Console.WriteLine($"\n\tI love making {product}");
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"\n\t{food} is my favorite snack");
+        }
+    }
+    public class Pig
+    {
+        public string name;
+        public string sound;
+        public string food;
+        public string product;
+        public Pig(string aName, string aSound, string aFood, string aProduct)
+        {
+            name = aName;
+            sound = aSound;
+            food = aFood;
+            product = aProduct;
+        }
+        public void Intro()
+        {
+            Console.WriteLine($"\n\t{sound} my name is {name} lets grab some {product} ");
+        }
+        public void Speak()
+        {
+            Console.WriteLine($"\n\t{sound} out the way!");
+        }
+        public void Product()
+        {
+            Console.WriteLine($"\n\tI love making {product}");
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"\n\t{food} is my favorite snack");
+        }
+    }
+}
---
+++ MyFarmTest/UnitTest1.cs
@@ -0,0 +1,70 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFarm;
+
+namespace MyFarmTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+             void makeFarm()
+            {
+                Cow cow1 = new Cow("Big Bertha", "MOOO!!!", "Grass", "Jeep Wrangler");
+                Cow cow2 = new Cow("Daisy", "MOOO!!!", "McDonalds", "Boats");        //The cows
+                Cow cow3 = new Cow("Billy", "MOOO!!!", "Subway", "Oranges");
+                Cow cow4 = new Cow("William", "MOOO!!!", "KFC", "Bananas");
+
+                Horse horse1 = new Horse("Mr.Ed", "Hello", "Hay", "Airplanes");
+                Horse horse2 = new Horse("Mr.Johnson", "Hello", "Chocolate", "Lumber");   //The horses
+                Horse horse3 = new Horse("Mr.Middleton", "Hello", "Ice Cream", "Oil");
+                Horse horse4 = new Horse("Mr.Jenkins", "Hello", "Bread", "Iron");
+
+                Pig pig1 = new Pig("Steven", "Oink!", "sushi", "bananas");
+                Pig pig2 = new Pig("Wilbur", "Oink!", "salads", "silver");        //The pigs
+                Pig pig3 = new Pig("Kyle", "Oink!", "burritos", "internet");
+                Pig pig4 = new Pig("Chad", "Oink!", "tacos", "gasoline");
+
+                Chicken chicken1 = new Chicken("Mr. Cluckles", "Hola", "seeds", "Chicken-Fil-a");
+                Chicken chicken2 = new Chicken("Mr. Smith", "Hola", "peanut butter", "KFC");      //The chickens
+                Chicken chicken3 = new Chicken("Mr. Brad", "Hola", "bacon", "brea");
+                Chicken chicken4 = new Chicken("Mr. Tyler", "Hola", "beef jerky", "Wal-Mart");
+
+                string retry = "No";
+
+                do
+                {
+                    Console.WriteLine("\n\tWhat animal would you want to meet?");
+                    Console.WriteLine("\n\tYou can meet my Horse, Cow, Chicken, or Pig!");
+                    string animal = Console.ReadLine();
+                    switch (animal)
+                    {
+                        case "Horse":
+                            horse1.Intro();
+                            break;
+                        case "Cow":
+                            cow2.Eat();
+                            break;
+                        case "Chicken":
+                            chicken3.Speak();
+                            break;
+                        case "Pig":
+                            pig4.Product();
+                            break;
+                        default:
+                            Console.WriteLine("\n\tWould you like to try again?");
+                            retry = Console.ReadLine();
+                            Console.WriteLine("\n\tI'll take that as a yes!");
+                            break;
+                    }
+                } while (retry != "No");
+            }
+        }
+    }
+}
